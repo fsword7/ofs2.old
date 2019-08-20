b68d197c9a62f214be83df7e8445446a0216d51e
Timothy Stark <fsword007@gmail.com>
2019-08-20 18:09:42 -0400

    New Camera package

--- src/engine/player.h
+++ src/engine/player.h
@@ -7,6 +7,21 @@
 
 #pragma once
 
+class Camera
+{
+public:
+    Camera() = default;
+    ~Camera() = default;
+
+    inline vec3f_t getPosition() const { return camPosition; };
+    inline quatf_t getRotation() const { return camRotation; };
+    
+private:
+    vec3f_t camPosition;
+    quatf_t camRotation;
+    float   fov; 
+}
+
 class Player
 {
 public:
