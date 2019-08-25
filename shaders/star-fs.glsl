e433ed8b556770254a91820083fe5008528f2008
Timothy Stark <fsword007@gmail.com>
2019-08-25 18:37:39 -0400

    New GLSL particle routines.

---
+++ shaders/star-fs.glsl
@@ -0,0 +1,11 @@
+#version 400
+
+in vec4  starColor;
+out vec4 fragColor;
+
+uniform sampler2D starTex;
+
+void main()
+{
+    fragColor = texture(starTex, gl_PointCoord) * starColor;
+}
---
+++ shaders/star-vs.glsl
@@ -0,0 +1,14 @@
+#version 400
+
+layout (location = 0) in vec3  vPosition;
+layout (location = 1) in vec3  vColor;
+layout (location = 2) in float vSize;
+
+out vec4 starColor;
+
+void main()
+{
+    gl_Position  = mvp * vPosition;
+    gl_PointSize = vSize;
+    starColor = vec4(vColor, 1.0f);
+}
