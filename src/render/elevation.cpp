1e47eb625ddd7978322e47b4c9a1896a9cd1ecec
Timothy Stark <fsword007@gmail.com>
2019-09-09 19:57:08 -0400

    Initial elevation data handling.

---
+++ src/render/elevation.cpp
@@ -0,0 +1,13 @@
+/*
+ * elevation.cpp - Terrain manager package (elevation)
+ *
+ *  Created on: Sep 9, 2019
+ *      Author: Tim Stark
+ */
+
+#include "main/core.h"
+#include "engine/object.h"
+#include "render/gl/texture.h"
+#include "render/render.h"
+#include "render/terrain.h"
+
