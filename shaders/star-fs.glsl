f4179c890b40741604f63dfdefa6d9893fa8b2e8
Timothy Stark <fsword007@gmail.com>
2019-09-29 12:29:38 -0400

    Now rendering circular points.

--- shaders/star-fs.glsl
+++ shaders/star-fs.glsl
@@ -7,5 +7,10 @@ out vec4 fragColor;
 
 void main()
 {
+	// Rendering circular points
+	vec2 circCoord = (gl_PointCoord * 2.0) - 1.0;
+	if (dot(circCoord, circCoord) > 1.0)
+		discard;
+
     fragColor = /* texture(starTex, gl_PointCoord) * */ starColor;
 }
