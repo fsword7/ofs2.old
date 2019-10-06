4dcc3c0a4db7827d86e3fd89e9151458bdf5bb1e
Timothy Stark <fsword007@gmail.com>
2019-10-06 16:21:19 -0400

    Initial orbit package

---
+++ src/ephem/orbit.cpp
@@ -0,0 +1,21 @@
+/*
+ * orbit.cpp
+ *
+ *  Created on: Oct 6, 2019
+ *      Author: Tim Stark
+ */
+
+#include "main/core.h"
+#include "ephem/orbit.h"
+
+vec3d_t EllipticalOrbit::getPosition(double jd)
+{
+	return vec3d_t(0, 0, 0);
+}
+
+vec3d_t EllipticalOrbit::getVelocity(double jd)
+{
+	return vec3d_t(0, 0, 0);
+}
+
+
---
+++ src/ephem/orbit.h
@@ -0,0 +1,45 @@
+/*
+ * orbit.h
+ *
+ *  Created on: Oct 6, 2019
+ *      Author: Tim Stark
+ */
+
+#pragma once
+
+class Orbit
+{
+public:
+	Orbit() = default;
+	virtual ~Orbit() = default;
+
+	virtual vec3d_t getPosition(double jd) = 0;
+	virtual vec3d_t getVelocity(double jd) = 0;
+};
+
+class EllipticalOrbit : public Orbit
+{
+public:
+	EllipticalOrbit() = default;
+	virtual ~EllipticalOrbit() = default;
+
+	vec3d_t getPosition(double jd) override;
+	vec3d_t getVelocity(double jd) override;
+
+protected:
+//	// Orbital elements
+//	double  T;  // Period [days]
+//	double  a;  // Semi-major Axis
+//	double  e;  // Eccentricity [rad]
+//	double  q;  // Perifocal distance
+//	double  i;  // Inclination [rad]
+//	double  Om; // Longitude of ascending node [rad]
+//	double  w;  // Argument of periapsis [rad]
+//	double  M0; // Mean anomaly at Epoch (time)
+//
+//	double  perigee;  // pericenter (perigee)
+//	double  epoch;
+//	double  GM;  // Standard gravitational parameter [GM]
+//
+//	quatd_t plane;
+};
