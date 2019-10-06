c48292ec91b3fdc385310f5f3acbf634e640e48b
Timothy Stark <fsword007@gmail.com>
2019-10-06 17:22:18 -0400

    Initial solar system package

---
+++ src/universe/system.cpp
@@ -0,0 +1,20 @@
+/*
+ * system.cpp - solar system package
+ *
+ *  Created on: Oct 6, 2019
+ *      Author: Tim Stark
+ */
+
+#include "main/core.h"
+#include "universe/body.h"
+#include "universe/star.h"
+#include "universe/system.h"
+
+using namespace ofs::universe;
+
+void System::addObject(Object *object)
+{
+
+}
+
+
---
+++ src/universe/system.h
@@ -0,0 +1,28 @@
+/*
+ * system.h - solar system package
+ *
+ *  Created on: Oct 6, 2019
+ *      Author: Tim Stark
+ */
+
+#pragma once
+
+namespace ofs::universe
+{
+	class System
+	{
+	public:
+		System() = default;
+		~System() = default;
+
+		inline CelestialStar *getStar(int idx)
+			{ return (idx < stars.size()) ? stars[idx] : nullptr; }
+
+		void addObject(Object *object);
+
+	private:
+		vector<CelestialStar *> stars;
+	//	PlanetarySystem *bodies = nullptr;
+	//	SystemTree *systemTree  = nullptr;
+	};
+}
