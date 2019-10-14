80a26e9eae2c729af5c5418ed73d8be146a939c7
Timothy Stark <fsword007@gmail.com>
2019-10-14 19:32:40 -0400

    Added Earth rotation model (only spinning but not precession yet).

--- src/ephem/rotation.cpp
+++ src/ephem/rotation.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "main/core.h"
+#include "universe/astro.h"
 #include "ephem/rotation.h"
 
 using namespace ofs::ephem;
@@ -15,9 +16,28 @@ quatd_t RotationModel::getEquatorRotation(double tjd) const
 	return quatd_t(1, 0, 0, 0);
 }
 
-//quatd_t RotationModel::getRotation(double tjd) const;
-//{
-//	return spin(tjd) * getEquatorRotation(tjd);
-//}
+vec3d_t RotationModel::getAngularVelocity(double tjd) const
+{
+	return vec3d_t(0, 0, 0);
+}
 
+quatd_t RotationModel::getRotation(double tjd) const
+{
+	return spin(tjd) * getEquatorRotation(tjd);
+}
 
+// ******** Earth Rotation Model ********
+
+quatd_t EarthRotationModel::spin(double tjd) const
+{
+	double t = tjd - ofs::astro::J2000;
+	double theta = (PI * 2) * (t * 24.0 / 23.9344694 - 259.853 / 360.0);
+
+	return yrot(-theta);
+//	return quatd_t(vec3d_t(0, -theta, 0));
+}
+
+quatd_t EarthRotationModel::getEquatorRotation(double tjd) const
+{
+	return quatd_t(1, 0, 0, 0);
+}
--- src/ephem/rotation.h
+++ src/ephem/rotation.h
@@ -14,7 +14,7 @@ namespace ofs::ephem
 	public:
 		virtual ~RotationModel() = default;
 
-//		quatd_t getRotation(double tjd) const;
+		quatd_t getRotation(double tjd) const;
 
 		virtual quatd_t spin(double tjd) const = 0;
 		virtual vec3d_t getAngularVelocity(double tjd) const;
@@ -22,5 +22,18 @@ namespace ofs::ephem
 
 		virtual double getPeroid() const  { return 0.0; }
 		virtual bool isPeroidic() const   { return false; }
+	};
+
+
+	class EarthRotationModel : public RotationModel
+	{
+	public:
+		EarthRotationModel() = default;
+
+		quatd_t spin(double tjd) const override;
+		quatd_t getEquatorRotation(double tjd) const override;
+
+		double getPeroid() const override { return 23.9344694 / 24.0; }
+		bool isPeroidic() const override { return true; }
 	};
 }
